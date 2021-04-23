e87cc9c8bf203f4a5ceea32bf92e9cbe44706388
riley817 <riley0422@icloud.com>
2021-04-23 10:46:23 +0900

    first commit

---
+++ src/main/java/com/example/oracleaq/OracleaqJmsApplication.java
@@ -0,0 +1,13 @@
+package com.example.oracleaq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OracleaqJmsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OracleaqJmsApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/oracleaq/OracleaqJmsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oracleaq;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OracleaqJmsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
